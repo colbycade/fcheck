70952f5573d3e7ba81e5d71debf9fc79613b48fc
colbycade <colbycade@gmail.com>
2025-12-12 19:20:28 -0600

    definitions and setup

---
+++ submit/fcheck.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>  // for mmap
+
+#include "fcheck.h"  // includes xv6 definitions
+
+#define BLOCK_SIZE (BSIZE)
+
+int main(int argc, char *argv[]) {
+    // --- SETUP ---
+    int fsfd;
+    char *addr;
+    struct stat st;
+    struct superblock *sb;
+    struct dinode *dip;
+    struct dirent *de;
+
+    // Usage check
+    if (argc != 2) {
+        fprintf(stderr, "Usage: fcheck <file_system_image>\n");
+        exit(1);
+    }
+
+    // Open the file system image
+    fsfd = open(argv[1], O_RDONLY);
+    if (fsfd < 0) {
+        fprintf(stderr, "image not found.\n");
+        exit(1);
+    }
+
+    // Get file system size
+    if (fstat(fsfd, &st) < 0) {
+        perror("fstat failed\n");
+        exit(1);
+    }
+
+    // Map the image into memory
+    addr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fsfd, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap failed\n");
+        exit(1);
+    }
+
+    // Read the superblock (block 1)
+    sb = (struct superblock *) (addr + 1 + BLOCK_SIZE);
+
+    // Read the inodes (inode table starts at block 2)
+    dip = (struct dinode *) (addr + IBLOCK((uint)0) * BLOCK_SIZE); 
+
+    // --- VERIFY CONSISTENCY RULES ---
+
+
+    // --- CLEANUP ---
+    munmap(addr, st.st_size);
+    close(fsfd);
+    return 0; //success
+}
---
+++ submit/fcheck.h
@@ -0,0 +1,56 @@
+// This header file contains the xv6 definitions we need for fcheck.c
+
+// --- xv6 type definitions (copied from types.h) ---
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
+
+// --- xv6 file system definitions (copied from fs.h) ---
+
+// Block 0 is unused.
+// Block 1 is super block.
+// Inodes start at block 2.
+
+#define ROOTINO 1  // root i-number
+#define BSIZE 512  // block size
+
+// File system super block
+struct superblock {
+  uint size;         // Size of file system image (blocks)
+  uint nblocks;      // Number of data blocks
+  uint ninodes;      // Number of inodes.
+};
+
+#define NDIRECT 12
+#define NINDIRECT (BSIZE / sizeof(uint))
+#define MAXFILE (NDIRECT + NINDIRECT)
+
+// On-disk inode structure
+struct dinode {
+  short type;           // File type
+  short major;          // Major device number (T_DEV only)
+  short minor;          // Minor device number (T_DEV only)
+  short nlink;          // Number of links to inode in file system
+  uint size;            // Size of file (bytes)
+  uint addrs[NDIRECT+1];   // Data block addresses
+};
+
+// Inodes per block.
+#define IPB           (BSIZE / sizeof(struct dinode))
+
+// Block containing inode i
+#define IBLOCK(i)     ((i) / IPB + 2)
+
+// Bitmap bits per block
+#define BPB           (BSIZE*8)
+
+// Block containing bit for block b
+#define BBLOCK(b, ninodes) (b/BPB + (ninodes)/IPB + 3)
+
+// Directory is a file containing a sequence of dirent structures.
+#define DIRSIZ 14
+
+struct dirent {
+  ushort inum;
+  char name[DIRSIZ];
+};
